06eb2dce6651f2ca179f1aa2e4bf3f29d8102719
Amelia Nelson <153486779+only-agape@users.noreply.github.com>
2023-12-13 13:51:53 +0700

    Create book.py

---
+++ book.py
@@ -0,0 +1,14 @@
+# book.py
+
+class Book:
+    def __init__(self, book_id, title, author, price):
+        self.book_id = book_id
+        self.title = title
+        self.author = author
+        self.price = price
+
+    def display_info(self):
+        print(f"Book ID: {self.book_id}")
+        print(f"Title: {self.title}")
+        print(f"Author: {self.author}")
+        print(f"Price: ${self.price}")
